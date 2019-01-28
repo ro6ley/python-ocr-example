89c6dfd2761d6383d010fbff382fab41d4ac5e98
Robley <robleyadrian@gmail.com>
2019-01-29 00:25:26 +0300

    add the OCR script.

---
+++ ocr_core.py
@@ -0,0 +1,18 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text  # Then we will print the text in the image
+
+
+print("")
+print(ocr_core('images/ocr_image_1.png'))
+print("")
