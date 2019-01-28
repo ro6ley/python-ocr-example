07f8bee4228de5a0bd86e5804c91f910b0b5d18a
Robley <robleyadrian@gmail.com>
2019-01-29 00:25:11 +0300

    add the Flask application file.

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask, render_template, request
+
+from ocr_core import ocr_core
+
+
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        print(UPLOAD_FOLDER)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            # file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+
+            # call the OCR function on it
+            extracted_text = ocr_core(file)
+
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run()
