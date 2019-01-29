68817e52e00c5444eedab357c01158f6c0cd3a3b
Robley <robleyadrian@gmail.com>
2019-01-29 14:45:01 +0300

    minor fixes

--- ocr_core.py
+++ ocr_core.py
@@ -11,8 +11,3 @@ def ocr_core(filename):
     """
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text  # Then we will print the text in the image
-
-
-print("")
-print(ocr_core('images/ocr_image_1.png'))
-print("")
